dad4a8e097b4ef4064f1abfd4935f0b12de8e97d
AlbinEriksson02 <albin.eriksson3@elev.ga.ntig.se>
2021-09-02 14:03:48 +0200

    facicon Added

--- florist-celeber/js/scripts.js
+++ florist-celeber/js/scripts.js
@@ -41,10 +41,10 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-    //Mobile navbar open/close on menu click
+    
     const navbarMenu = document.getElementById('navbarMenu');
     const navbarCollapse = document.getElementById('navbarResponsive');
-    let ariaExpanded = navbarMenu.getAttribute("aria-expanded");
+    const ariaExpanded = navbarMenu.getAttribute("aria-expanded");
     navbarMenu.onclick = function(){
         if (navbarCollapse.classList.contains('show')) {
             navbarToggler.classList.remove('focus')
@@ -53,6 +53,7 @@ window.addEventListener('DOMContentLoaded', event => {
             return
         }
         navbarToggler.classList.add('focus')
+        console.log('hej'); 
         navbarCollapse.classList.add('show')
         ariaExpanded = 'true';
     }
