eea9b83d3d8511a5090cb51456cc4db97379e107
AlbinEriksson02 <albin.eriksson3@elev.ga.ntig.se>
2021-09-20 15:59:35 +0200

    More drivers and social links

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 import time
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("http://www.itgwebb.se/klass/webb2/christoffer/dev/florist-celeber")
 
 
@@ -44,10 +46,12 @@ def checkForWelcomeMessage():
     checkForText("FLORIST CELEBER")
     print("checkForWelcomeMessage test completed")
 
+#Staff Introduction
 def checkForStaffIntroduction():
     checkForText("Detta är")
     checkForText("VÅR UNDERBARA PERSONAL")
     print("checkForStaffIntroduction test completed")
+
 #Find us Info
 def checkForAddress():
     checkForText("Fjällgatan 32H")
@@ -64,9 +68,7 @@ def checkForContact():
 #Header Info
 def headerInfo():
     checkForText("INFO")
-    checkForText("ÖPPETTIDER")
-    checkForText("HITTA OSS")
-    checkForText("KONTAKTUPPGIFTER")
+    checkForText("HEM")
     checkForText("PERSONAL")
     print("headerInfo test completed")
 
@@ -87,56 +89,86 @@ def staffPageInfo():
 
 #staff page pictures
 def staffPagePictures():
-    driver.find_element_by_xpath("//img[@alt='Fredrik Örtqvist']")
+    driver.find_element_by_xpath("//img[@alt='Fredrik Örtqvist']") #Locates The Alt @ in all Image @ If the alt Is correct the image shall be there. Proof can be seen In screenshot
     driver.find_element_by_xpath("//img[@alt='Anna Pettersson']")
     driver.find_element_by_xpath("//img[@alt='Örjan Johansson']")
     print("Staff page images test Completed")
+
+#social media links 
+def socialMediaLinks():
+    facebookLink = driver.find_element_by_id("Facebook") #Locate id Facebook
+    driver.execute_script("window.scrollTo(0, 100000)") #Scroll to bottom of page
+    time.sleep(1)
+    facebookLink.click()    #click link
+    time.sleep(3)
+    driver.back()    #go back to main site
+    print("facebookLink Test Passed")
+
+    time.sleep(1)
+    twitterLink = driver.find_element_by_id("Twitter") ##SAME AS ABOVE
+    driver.execute_script("window.scrollTo(0, 100000)")
+    time.sleep(1)
+    twitterLink.click()
+    time.sleep(3)
+    driver.back()
+    print("twitterLink Test Passed")
+
+    time.sleep(1)
+    instagramLink = driver.find_element_by_id("Instagram")
+    driver.execute_script("window.scrollTo(0, 100000)")
+    time.sleep(1)
+    instagramLink.click()
+    time.sleep(3)
+    driver.back()
+    print("instagramLink Test Passed")
+
+    print("Social media links test Completed")
 
 #OpeningHoursLive
 def openingHourslive():
 
     dates = [
-        ["new Date('13 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #monday before just opening
-        ["new Date('13 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #monday before soon opening
-        ["new Date('13 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #monday after opening
-        ["new Date('13 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #monday before closing
-        ["new Date('13 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #monday after closing
+        ["new Date('13 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #monday more than 30 minutes before opening
+        ["new Date('13 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #monday less than 30 minutes before opening
+        ["new Date('13 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #monday just after opening
+        ["new Date('13 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #monday less than 1 hour before closing
+        ["new Date('13 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #monday just after closing
 
-        ["new Date('14 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #tuesday before just opening
-        ["new Date('14 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #tuesday before soon opening
-        ["new Date('14 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #tuesday after opening
-        ["new Date('14 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #tuesday before closing
+        ["new Date('14 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #tuesday more than 30 minutes before opening
+        ["new Date('14 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #tuesday less than 30 minutes before opening
+        ["new Date('14 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #tuesday just after opening
+        ["new Date('14 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #tuesday less than 1 hour before closing
         ["new Date('14 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #tuesday after closing
 
-        ["new Date('15 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #wednesday before just opening
-        ["new Date('15 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #wednesday before soon opening
-        ["new Date('15 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #wednesday after opening
-        ["new Date('15 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #wednesday before closing
+        ["new Date('15 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #wednesday more than 30 minutes before opening
+        ["new Date('15 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #wednesday less than 30 minutes before opening
+        ["new Date('15 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #wednesday just after opening
+        ["new Date('15 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #wednesday less than 1 hour before closing
         ["new Date('15 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #wednesday after closing
 
-        ["new Date('16 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #thursday before just opening
-        ["new Date('16 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #thursday before soon opening
-        ["new Date('16 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #thursday after opening
-        ["new Date('16 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #thursday before closing
+        ["new Date('16 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #thursday more than 30 minutes before opening
+        ["new Date('16 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #thursday less than 30 minutes before opening
+        ["new Date('16 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #thursday just after opening
+        ["new Date('16 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #thursday less than 1 hour before closing
         ["new Date('16 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #thursday after closing
 
-        ["new Date('17 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #friday before just opening
-        ["new Date('17 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #friday before soon opening
-        ["new Date('17 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #friday after opening
-        ["new Date('17 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #friday before closing
+        ["new Date('17 Sep 2021 9:25:00 GMT+2')", "Öppnar idag kl 10"], #friday more than 30 minutes before opening
+        ["new Date('17 Sep 2021 9:55:00 GMT+2')", "Öppnar snart"], #friday less than 30 minutes before opening
+        ["new Date('17 Sep 2021 10:05:00 GMT+2')", "Öppet just nu"], #friday just after opening
+        ["new Date('17 Sep 2021 15:05:00 GMT+2')", "Stänger snart"], #friday less than 1 hour before closing
         ["new Date('17 Sep 2021 18:05:00 GMT+2')", "Öppnar imorgon kl 12"], #friday after closing
 
-        ["new Date('18 Sep 2021 11:25:00 GMT+2')", "Öppnar idag kl 12"], #saturday before just opening
-        ["new Date('18 Sep 2021 11:55:00 GMT+2')", "Öppnar snart"], #saturday before soon opening
-        ["new Date('18 Sep 2021 12:05:00 GMT+2')", "Öppet just nu"], #saturday after opening
-        ["new Date('18 Sep 2021 14:05:00 GMT+2')", "Stänger snart"], #saturday before closing
+        ["new Date('18 Sep 2021 11:25:00 GMT+2')", "Öppnar idag kl 12"], #saturday more than 30 minutes before opening
+        ["new Date('18 Sep 2021 11:55:00 GMT+2')", "Öppnar snart"], #saturday less than 30 minutes before opening
+        ["new Date('18 Sep 2021 12:05:00 GMT+2')", "Öppet just nu"], #saturday just after opening
+        ["new Date('18 Sep 2021 14:05:00 GMT+2')", "Stänger snart"], #saturday less than 1 hour before closing
         ["new Date('18 Sep 2021 15:05:00 GMT+2')", "Öppnar på måndag kl 10"], #saturday after closing
-        
-        ["new Date('19 Sep 2021 9:25:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday before just opening
-        ["new Date('19 Sep 2021 9:55:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday before soon opening
-        ["new Date('19 Sep 2021 10:05:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday after opening
-        ["new Date('19 Sep 2021 15:05:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday before closing
-        ["new Date('19 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday after closing
+        #SUNDAY SHOULD BE CLOSED ALL DAY!!
+        ["new Date('19 Sep 2021 9:25:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday more than 30 minutes before normal opening time
+        ["new Date('19 Sep 2021 9:55:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday less than 30 minutes before normal opening time
+        ["new Date('19 Sep 2021 10:05:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday just after normal opening time
+        ["new Date('19 Sep 2021 15:05:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday less than 1 hour before normal closing time
+        ["new Date('19 Sep 2021 16:05:00 GMT+2')", "Öppnar imorgon kl 10"], #sunday after normal closing time
 
         ["new Date('13 Sep 2021 00:00:00 GMT+2')", "Öppnar idag kl 10"], # monday midnight
         ["new Date('14 Sep 2021 00:00:00 GMT+2')", "Öppnar idag kl 10"], # tuesday midnight
@@ -173,8 +205,7 @@ def screenShots(resolutions):
             # sets the resolution for the mobile emulation
             emulation = {
                 "deviceMetrics": { "width": res[0], "height": res[1], "pixelRatio": 1.0 },
-                "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
-            }
+                "userAgent": "Mozilla/5.0" }
         else:
             print(res + "invalid device type")
 
@@ -183,9 +214,7 @@ def screenShots(resolutions):
         # runs the emulation for different resolutions
         chrome_options.add_experimental_option("mobileEmulation", emulation)
 
-
         driver = webdriver.Chrome( options = chrome_options)
-
 
         driver.get(website) 
 
@@ -194,27 +223,27 @@ def screenShots(resolutions):
 
         driver.save_screenshot( "../../Screenshots/Image (" + dt_string  + str(res[0]) + " x " + str(res[1]) + ")" + ".jpg")
         
+        # Loads staff Page 
         driver.get(staffPage)
 
         time.sleep(3) 
 
+        # Saves images in map 2 steps back. format(time, resolution 1 x resolution 2)(.jpg can be changed to other image types)
         driver.save_screenshot( "../../Screenshots/Image (" + dt_string + str(res[0]) + " x " + str(res[1]) + ")" + " staffpage" + ".jpg")
 
         driver.close()
-
-
 
         
     print("Screenshots completed")
 
 #----RUN TESTS
+
+#CHROME
 # runs all tests for the website
-# runs driver separately from the screenshot function
 driver.get(website)
-# sleeps for 3 seconds to make sure site is fully loaded!
 time.sleep(3)
 
-
+# Runs tests
 testTitleName("Florist Celeber")
 checkForOpeningHours()
 checkForAddress()
@@ -222,11 +251,15 @@ checkForContact()
 headerInfo()
 footerInfo()
 checkForWelcomeMessage()
+socialMediaLinks()
+
 openingHourslive()
 
+# switch Page to staff page
 driver.get(staffPage)
 time.sleep(3) 
 
+# Runs Tests for staff page
 testTitleName("Florist Celeber")
 headerInfo()
 footerInfo()
@@ -234,6 +267,82 @@ footerInfo()
 staffPageInfo()
 staffPagePictures()
 checkForStaffIntroduction()
+
+socialMediaLinks()
 driver.close()
+
+#FIREFOX
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+
+driver.get(website)
+time.sleep(3)
+
+# Runs tests
+testTitleName("Florist Celeber")
+checkForOpeningHours()
+checkForAddress()
+checkForContact()
+headerInfo()
+footerInfo()
+checkForWelcomeMessage()
+socialMediaLinks()
+
+openingHourslive()
+
+# switch Page to staff page
+driver.get(staffPage)
+time.sleep(3) 
+
+# Runs Tests for staff page
+testTitleName("Florist Celeber")
+headerInfo()
+footerInfo()
+
+staffPageInfo()
+staffPagePictures()
+checkForStaffIntroduction()
+
+socialMediaLinks()
+driver.close()
+
+
+#Microsoft Edge 
+
+driver = webdriver.Edge()
+driver.maximize_window()
+
+driver.get(website)
+time.sleep(3)
+
+# Runs tests
+testTitleName("Florist Celeber")
+checkForOpeningHours()
+checkForAddress()
+checkForContact()
+headerInfo()
+footerInfo()
+checkForWelcomeMessage()
+socialMediaLinks()
+
+openingHourslive()
+
+# switch Page to staff page
+driver.get(staffPage)
+time.sleep(3) 
+
+# Runs Tests for staff page
+testTitleName("Florist Celeber")
+headerInfo()
+footerInfo()
+
+staffPageInfo()
+staffPagePictures()
+checkForStaffIntroduction()
+
+socialMediaLinks()
+driver.close()
+
 screenShots(screenResolution)
 print("ALL TESTS PASSED!")
